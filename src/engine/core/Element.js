aa396d2d403ae7b138b351968f7e36f43ac18fab
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2016-03-24 01:55:44 -0300

    Added isPointInside method to Element class

--- src/engine/core/Element.js
+++ src/engine/core/Element.js
@@ -1,194 +1,110 @@
 function Element() {
-    this.x = 0;
-    this.y = 0;
-    this.scaleW = 1;
-    this.scaleH = 1;
-    this.rotation = 0;
-    this.w = 0;
-    this.h = 0;
-    this.z = 0;
-    this.visible = true;
-    this.mouseInteract = false;
+	this.x = 0;
+	this.y = 0;
+	this.scaleW = 1;
+	this.scaleH = 1;
+	this.rotation = 0;
+	this.w = 0;
+	this.h = 0;
+	this.z = 0;
+	this.visible = true;
+	this.mouseInteract = false;
 }
 
 Element.prototype.draw = function () {
 };
 
 Element.prototype.getCorners = function () {
-    var coords = [];
+	var coords = [];
 
-    var posX = this.x;
-    var posY = this.y;
+	var posX = this.x;
+	var posY = this.y;
 
-    var rot = -this.rotation;
+	var rot = -this.rotation;
 
-    var w = this.w;
-    var h = this.h;
+	var w = this.w;
+	var h = this.h;
 
-    var scaleH = this.scaleH;
-    var scaleW = this.scaleW;
+	var scaleH = this.scaleH;
+	var scaleW = this.scaleW;
 
-    var top = posY + (h * scaleH) / 2;
-    var left = posX - (w * scaleW) / 2;
-    var right = posX + (w * scaleW) / 2;
-    var down = posY - (h * scaleH) / 2;
+	var top = posY + (h * scaleH) / 2;
+	var left = posX - (w * scaleW) / 2;
+	var right = posX + (w * scaleW) / 2;
+	var down = posY - (h * scaleH) / 2;
 
-    coords.push({x: right, y: top});
-    coords.push({x: left, y: top});
-    coords.push({x: left, y: down});
-    coords.push({x: right, y: down});
+	coords.push({x: right, y: top});
+	coords.push({x: left, y: top});
+	coords.push({x: left, y: down});
+	coords.push({x: right, y: down});
 
-    for (var i = 0; i < coords.length; i++) {
-        var originalX = coords[i].x - posX;
-        var originalY = coords[i].y - posY;
+	for (var i = 0; i < coords.length; i++) {
+		var originalX = coords[i].x - posX;
+		var originalY = coords[i].y - posY;
 
-        coords[i].x = originalX * Math.cos(rot) - originalY * Math.sin(rot);
-        coords[i].y = originalX * Math.sin(rot) + originalY * Math.cos(rot);
+		coords[i].x = originalX * Math.cos(rot) - originalY * Math.sin(rot);
+		coords[i].y = originalX * Math.sin(rot) + originalY * Math.cos(rot);
 
-        coords[i].x += posX;
-        coords[i].y += posY;
-    }
+		coords[i].x += posX;
+		coords[i].y += posY;
+	}
 
-    return coords;
+	return coords;
 };
 
 Element.prototype.setScale = function (n) {
-    this.scaleH = this.scaleW = parseFloat(n);
+	this.scaleH = this.scaleW = parseFloat(n);
 };
 
 (function () {
 
-    var label = 0;
+	function intersects(x1, y1, x2, y2, x3, y3, x4, y4) {
+		// src: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+		var denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+		var ret = 0;
 
-    var interLinha;
+		if (denominator !== 0) {
+			var pxNumerator = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
+			var pyNumerator = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
 
-    function intersects(x1, y1, x2, y2, x3, y3, x4, y4) {
-        // src: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
-        var denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-        var ret = 0;
+			var px = pxNumerator / denominator;
+			var py = pyNumerator / denominator;
 
-        if (denominator !== 0) {
-            var pxNumerator = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
-            var pyNumerator = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+			var tx1 = (px - x1) / (x2 - x1);
+			var ty1 = (py - y1) / (y2 - y1);
+			
+			var tx2 = (px - x3) / (x4 - x3);
+			var ty2 = (py - y3) / (y4 - y3);
+			
+			if (tx1 >= 0 && tx1 <= 1 && ty1 >= 0 && ty1 <= 1 &&
+				tx2 >= 0 && tx2 <= 1 && ty2 >= 0 && ty2 <= 1) {
+				ret = 1;
+			}
+		}
 
-            var px = pxNumerator / denominator;
-            var py = pyNumerator / denominator;
+		return ret;
+	}
 
-            console.clear();
-            console.log(px, py, x1, y1, x2, y2);
+	Element.prototype.isPointInside = function (x, y) {
 
-            console.log(x1 + " < " + px + " < " + x2);
-            console.log(y1 + " < " + py + " < " + y2);
+		var corners = this.getCorners();
+		var acc = 0;
 
-            if ((px >= x1 && px < x2 ||
-                    px > x2 && px <= x1) &&
-                    (py >= y1 && py < y2 ||
-                            py > y2 && py <= y1
-                            )) {
-                ret = 1;
-                interLinha.sx = px;
-                interLinha.sy = py;
-                interLinha.visible = true;
-            } else {
-                interLinha.visible = false;
-            }
-        }
+		var sx = x - this.w * 100;
+		var sy = y;
 
-        return ret;
-    }
+		var rotatedY0 = sx * Math.sin(this.rotation) + sy * Math.cos(this.rotation);
+		var rotatedX0 = sx * Math.cos(this.rotation) - sy * Math.sin(this.rotation);
 
-    var linha = 0;
+		var rotatedX1 = x;
+		var rotatedY1 = y;
 
-    var l = [];
+		acc += intersects(corners[0].x, corners[0].y, corners[1].x, corners[1].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
+        acc += intersects(corners[1].x, corners[1].y, corners[2].x, corners[2].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
+        acc += intersects(corners[3].x, corners[3].y, corners[0].x, corners[0].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
+        acc += intersects(corners[2].x, corners[2].y, corners[3].x, corners[3].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
 
-    Element.prototype.isPointInside = function (x, y) {
-
-        label = 0;
-
-        var corners = this.getCorners();
-        var acc = 0;
-
-        var sx = x - this.w * 100;
-        var sy = y;
-
-        var rotatedY0 = sx * Math.sin(this.rotation) + sy * Math.cos(this.rotation);
-        var rotatedX0 = sx * Math.cos(this.rotation) - sy * Math.sin(this.rotation);
-
-        var rotatedX1 = x;
-        var rotatedY1 = y;
-
-        if (linha++ === 0) {
-
-            interLinha = new Line({g: 255});
-            GameON.add(interLinha);
-
-            interLinha.sx = interLinha.sy = 0;
-            interLinha.visible = false;
-
-            var instanceLine = new Line({r: 255, g: 255});
-            GameON.add(instanceLine);
-
-            l.push(instanceLine);
-
-            instanceLine = new Line({r: 255, g: 255});
-
-            GameON.add(instanceLine);
-
-            l.push(instanceLine);
-
-            instanceLine = new Line({r: 255, g: 255});
-
-            GameON.add(instanceLine);
-
-            l.push(instanceLine);
-
-            instanceLine = new Line({r: 255, g: 255});
-
-            GameON.add(instanceLine);
-
-            l.push(instanceLine);
-
-            instanceLine = new Line({b: 255, g: 255});
-
-            GameON.add(instanceLine);
-
-            l.push(instanceLine);
-        }
-
-        l[0].sx = corners[0].x;
-        l[0].sy = corners[0].y;
-        l[0].ex = corners[1].x;
-        l[0].ey = corners[1].y;
-
-
-        l[1].sx = corners[1].x;
-        l[1].sy = corners[1].y;
-        l[1].ex = corners[2].x;
-        l[1].ey = corners[2].y;
-
-        l[2].sx = corners[3].x;
-        l[2].sy = corners[3].y;
-        l[2].ex = corners[0].x;
-        l[2].ey = corners[0].y;
-
-
-        l[3].sx = corners[2].x;
-        l[3].sy = corners[2].y;
-        l[3].ex = corners[3].x;
-        l[3].ey = corners[3].y;
-
-
-        l[4].sx = rotatedX0;
-        l[4].sy = rotatedY0;
-        l[4].ex = rotatedX1;
-        l[4].ey = rotatedY1;
-
-        acc += intersects(corners[0].x, corners[0].y, corners[1].x, corners[1].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
-//        acc += intersects(corners[1].x, corners[1].y, corners[2].x, corners[2].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
-//        acc += intersects(corners[3].x, corners[3].y, corners[0].x, corners[0].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
-//        acc += intersects(corners[2].x, corners[2].y, corners[3].x, corners[3].y, rotatedX0, rotatedY0, rotatedX1, rotatedY1);
-
-        return acc % 2 ? false : true;
-    };
+		return acc % 2 ? false : true;
+	};
 
 })();
