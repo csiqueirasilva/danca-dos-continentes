299a66798cb265aa5a09f5b9a6f1e45969fbf0d0
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2016-04-01 14:45:02 -0300

    Added snap behavior to Piece class

--- src/js/Main.js
+++ src/js/Main.js
@@ -21,6 +21,13 @@ var mapNames = {
 	'rodinia': 'Rodínia'
 };
 
+var backgrounds = {};
+var pieces = [];
+
+var mapCoords = {
+	'futuro': {"NA":{"x":-0.18637977164272845,"y":0.23117491532868106},"China":{"x":0.24571767250782575,"y":0.1720885745888408},"AFR1":{"x":0.048207442490614504,"y":0.19379390859972642},"SA2":{"x":-0.27242512203256397,"y":-0.031930093609597324},"AFR3":{"x":0.11041210450597441,"y":-0.022775481677659543},"AUS":{"x":0.3175262690116946,"y":-0.03878383317294758},"SA3":{"x":-0.30253377199357295,"y":-0.13245170133154127},"EU":{"x":0.18870568335829407,"y":0.28331003006977035},"SA1":{"x":-0.33057147457608227,"y":-0.029115428793955785},"AFREU":{"x":0.11763335147560428,"y":0.27595204857035516},"AFR2":{"x":0.09164785483328294,"y":0.06870061602719109},"India":{"x":0.21073831328812148,"y":0.09104912288686198},"Antartida":{"x":-0.08066113777938186,"y":-0.3146024665293112}}
+};
+
 var pieceWrapper = new Element();
 pieceWrapper.visible = false;
 pieceWrapper.z = 10;
@@ -32,18 +39,33 @@ GameON.add(gameWrapper);
 
 var scale = (GameON.Camera.w * 0.9) / 457;
 
-for(var i = 0; i < pieceNames.length; i++) {
-	var square = new Piece({
-		imgPath: 'imgs/futuro/pecas-futuro/' + pieceNames[i] + '.png'
+for (var key in mapNames) {
+	var bg = new SquareImage({
+		imgPath: 'imgs/' + key + '/globo-com-fundo' + '.png'
 	});
-	
+
+	bg.visible = false;
+	backgrounds[key] = bg;
+	gameWrapper.add(bg);
+	bg.setScale(scale);
+	bg.z = 0;
+}
+
+for (var i = 0; i < pieceNames.length; i++) {
+	var square = new Piece({
+		imgPath: 'imgs/pecas/' + pieceNames[i] + '.png'
+	});
+
+	square.name = pieceNames[i];
+
 	square.x = (Math.random() * 0.8 - 0.4) * GameON.Camera.w;
 	square.y = (Math.random() * 0.8 - 0.4) * GameON.Camera.h;
 
 	square.setScale(scale);
-	
+
 	square.rotation = Piece.prototype.rotationAngle * parseInt(Math.random() * 100);
-	
+
+	pieces.push(square);
 	pieceWrapper.add(square);
 }
 
@@ -64,32 +86,33 @@ var idx = 0;
 var mapOptionStart = 20;
 var mapOptionSize = 5;
 
-for(var key in mapNames) {
+for (var key in mapNames) {
 	var link = new ClickableText({
 		txt: mapNames[key]
 	});
 
+	link._key = key;
+
 	link.setSize(mapOptionSize);
 
 	link.setPosition(0, mapOptionStart + idx * -mapOptionSize);
-	
+
 	idx += 1.5;
-	
+
 	link.clickCallback = function () {
+		var key = this._key;
+		
+		for(var i = pieces.length - 1; i >= 0; i--) {
+			pieces[i].target = mapCoords[key][pieces[i].name];
+			pieces[i].target.x *= GameON.Canvas.w;
+			pieces[i].target.y *= GameON.Canvas.h;
+		}
+		
+		backgrounds[key].visible = true;
 		pieceWrapper.visible = true;
 		mainMenuWrapper.visible = false;
-		
-		var bg = new SquareImage({
-			imgPath: 'imgs/futuro/' + 'globo-com-fundo' + '.png'
-		});
-		
-		bg.setScale(scale);
-		
-		bg.z = 0;
-		
-		gameWrapper.add(bg);
 	};
-	
+
 	mainMenuWrapper.add(link);
 }
 
--- src/js/elements/Piece.js
+++ src/js/elements/Piece.js
@@ -1,11 +1,16 @@
 function Piece(ops) {
 	ops = ops || {};
 	SquareImage.apply(this, arguments);
+	this.target = {x: 0, y: 0};
 }
 
 Piece.prototype = Object.create(SquareImage.prototype);
 
 Piece.prototype.rotationAngle = 6 * Math.PI / 180;
+
+Piece.prototype.snapRadius = (5 / (1920 * 1920 + 1080 * 1080)) * (GameON.Canvas.w * GameON.Canvas.w + GameON.Canvas.h * GameON.Canvas.h);
+
+Piece.prototype.snapRotation = 0.0333;
 
 Piece.prototype.mouseDown = Clickable.prototype.mouseDown;
 
@@ -21,14 +26,40 @@ Piece.prototype.mouseOut = function () {
 	this._parent.moveRenderOrder(this, this.z - 1);
 };
 
+Piece.prototype.onSnapPosition = function () {
+	var rot = (this.rotation % (Math.PI * 2)) / (Math.PI * 2);
+	if(rot < 0) {
+		rot += 1;
+	}
+	var ret = false;
+	if (rot >= (1 - this.snapRotation) || rot <= this.snapRotation) {
+		var x = this.target.x - this.x;
+		var y = this.target.y - this.y;
+		var r = Math.sqrt(x * x + y * y);
+		ret = r <= this.snapRadius;
+	}
+	return ret;
+};
+
 Piece.prototype.mouseMove = function (mouseX, mouseY) {
-	//image has opacity at this position, interact
-	this.draggable = Clickable.prototype.isDown.call(this) && this.inVisiblePixel(mouseX, mouseY);
+
+	if (Clickable.prototype.isDown.call(this)) {
+		//image has opacity at this position, interact
+		this.draggable = this.inVisiblePixel(mouseX, mouseY);
+		if (this.onSnapPosition()) {
+			console.log(this.name);
+		}
+	}
+
 };
 
 Piece.prototype.mouseWheel = function (mouseX, mouseY, direction, ev) {
 	if (this.inVisiblePixel(mouseX, mouseY)) {
 		var rotateSpeed = this.rotationAngle * direction;
 		this.rotation += rotateSpeed;
+		
+		if (this.onSnapPosition()) {
+			console.log(this.name);
+		}
 	}
 };
